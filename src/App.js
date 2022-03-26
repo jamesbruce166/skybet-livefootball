a9c9438b6887e9cba7cfc6764efe13db5eda20ae
James Erringham-Bruce <james@qcutapp.com>
2022-03-26 02:39:03 +0000

    Setup initial home screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages';
 function App() {
-	return <div></div>;
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
